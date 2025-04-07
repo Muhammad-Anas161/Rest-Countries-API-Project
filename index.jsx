97e55895cdedcbf143cd00545e762d83c0092990
Muhammad Anus <anasshahid161@gmail.com>
2025-04-07 23:42:58 +0500

    update index

--- index.jsx
+++ index.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Contact from './components/Contact'
 import Home from './components/Home'
 import Error from './components/Error'
 import CountryDetail from './components/CountryDetail'
